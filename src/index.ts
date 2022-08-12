a53bd1f72b83296c6cdffc32d34a916e39eb8a5a
Nanako <00sh.chiha+github@gmail.com>
2022-08-13 03:17:50 +0900

    ngl this pretty fun

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ let Canvas = document.getElementById('canvas') as HTMLCanvasElement
 let ctx = Canvas.getContext('2d') as CanvasRenderingContext2D
 
 let objects: bark.GameObject[] = []
-for (let i = 0; i < 50; i++) {
+for (let i = 0; i < 200; i++) {
   objects.push({
     position: {
       x: Math.random() * Canvas.width,
@@ -29,13 +29,11 @@ function mainGame () {
   objects.forEach(object => {
     object.position.y += Math.random() * 4 - 2 * gravity
     object.position.x += Math.random() * 4 - 2
-    if (object.position.y > Canvas.height) {
-      object.position.y = 0
-    }
+    if (object.position.y > Canvas.height) object.position.y = 0
     if (object.position.x > Canvas.width) object.position.x = -Canvas.width
     if (object.position.x < -Canvas.width) object.position.x = Canvas.width
-    object.size.x = Math.random() * 5
-    object.size.y = Math.random() * 5
+    object.size.x = Math.random() * 2
+    object.size.y = Math.random() * 2
   })
 }
 
