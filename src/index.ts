26c0cfaf2fef287c21be56a803c007df786e86f4
Nanako <00sh.chiha+github@gmail.com>
2022-08-14 16:45:36 +0900

    not giving up on 3d 😠💢💢

--- src/index.ts
+++ src/index.ts
@@ -3,33 +3,7 @@ import * as bark from './Enjin'
 let Canvas = document.getElementById('canvas') as HTMLCanvasElement
 let ctx = Canvas.getContext('2d') as CanvasRenderingContext2D
 
-let objects: bark.GameObject[] = []
-for (let i = 0; i < 2000; i++) {
-  objects.push({
-    position: {
-      x: Math.random() * Canvas.width,
-      y: Math.random() * Canvas.height,
-    },
-    size: {
-      x: Math.random() * 5,
-      y: Math.random() * 5
-    },
-    color: { r: Math.random() * 255, g: Math.random() * 255, b: Math.random() * 255, a: 1 },
-    type: 'arc',
-    extra: {
-      gravity: 30
-    }
-  })
-}
-
-let game = new bark.Game(60, ctx)
-game.scene.objects = objects
-let gravity = 50
-function mainGame () {
-  game.draw()
-  game.scene.objects.forEach(object => {
-    object.position.y += Math.sin(object.position.x / 100) * object.extra.gravity
-    object.position.x += Math.cos(object.position.y / 100) * object.extra.gravity
-  })
-}
-setInterval(mainGame, 1000 / game.fps)
+let Game = new bark.Game(60, ctx)
+let go: bark.GameObject = { type: 'rect', position: { x: 239847239, y: 0, z: 0 }, size: { x: 100, y: 100 }, color: { r: 0, g: 0, b: 255, a: 1 } }
+Game.scene.AddObject(go)
+Game.draw()
