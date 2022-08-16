97b455da50850e761cc2970488f66314fa9973a7
Nanako <00sh.chiha+github@gmail.com>
2022-08-16 21:17:40 +0900

    dependencies for gh pages and stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,10 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-
-const path = require("path");
-
-const isProduction = process.env.NODE_ENV == "production";
-
 const config = {
   entry: "./src/index.ts",
   output: {
-    path: path.resolve(__dirname, "dist"),
+    publicPath: "/bark-engine",
+    path: "/dist",
   },
   plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
   module: {
     rules: [
@@ -24,9 +17,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
   resolve: {
@@ -35,10 +25,6 @@ const config = {
 };
 
 module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
-  } else {
-    config.mode = "development";
-  }
+  config.mode = process.env.NODE_ENV || "development";
   return config;
 };
